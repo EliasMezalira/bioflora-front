c7c02cc8b649c065573048f4224d1f703bf62a16
Daniel Abrahao <daniel@preface.com.br>
2026-08-07 21:04:35 -0300

    build: configura coleta de cobertura nos testes

--- jest.config.ts
+++ jest.config.ts
@@ -3,5 +3,9 @@ import { createCjsPreset } from 'jest-preset-angular/presets';
 
 export default {
   ...createCjsPreset(),
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.test.js'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['html', 'text-summary'],
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
 } satisfies Config;
